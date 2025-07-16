00a4d34569886ba7b9c9535627c6b719dd2ebb06
Samir Otero <samirotero97@gmail.com>
2025-07-16 17:27:46 -0500

    modified routes and views. Will work on kaminari, as it is not functioning as expected

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get "episodes/index"
-  get "episodes/show"
-  get "locations/index"
-  get "locations/show"
-  get "characters/index"
-  get "characters/show"
+  resources :characters, only: [:index, :show]
+  resources :locations, only: [:index, :show]
+  resources :episodes, only: [:index, :show]
+  get "about", to: "pages#about", as: :about
+
+  root "characters#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
